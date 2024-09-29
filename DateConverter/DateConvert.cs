5f9b15b986b1bcdd61e2f47cd149a08c1cfa7b53
Anil <anil.l@camdigitaltech.com>
2024-09-29 17:57:25 +0530

    initial commit

---
+++ DateConverter/DateConvert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateConverter
+{
+    public class DateConvert
+    {
+        public static string Convert(string inputDate, int addDays)
+        {
+            string outputDate = string.Empty;
+
+            DateTime date;
+            if (DateTime.TryParseExact(inputDate, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out date))
+            {
+                date = DateTime.ParseExact(inputDate, "dd/MM/yyyy", null);
+                date = date.AddDays(addDays);
+                outputDate = date.ToString("dd'/'MM'/'yyyy");
+            }
+
+            return outputDate;
+        }
+    }
+}
---
+++ DateConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DateConverter;
+
+Console.WriteLine("Date convert");
+
+/// <summary>
+/// global variables
+/// </summary>
+string dateInput = string.Empty;
+int numberOfDays = 0;
+
+Console.WriteLine("Enter date: ");
+dateInput = Console.ReadLine();
+
+Console.WriteLine("Enter number of days to be added: ");
+numberOfDays = Convert.ToInt32(Console.ReadLine());
+
+string outDate = DateConvert.Convert(dateInput, numberOfDays);
+Console.WriteLine(string.Format("New Date: {0}", outDate));
+
+Console.ReadKey();
